235d75f27a855f0c5e97679f94a859e38d2a8664
Muza2001 <muza5660@gmail.com>
2023-02-03 10:56:28 +0500

    some mistake fixed

--- src/main/java/digital/one/config/SwaggerConfig.java
+++ src/main/java/digital/one/config/SwaggerConfig.java
@@ -44,7 +44,7 @@ public class SwaggerConfig {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return List.of(new SecurityReference("JWT", authorizationScopes));
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
     }
 
     private ApiInfo metadata() {
